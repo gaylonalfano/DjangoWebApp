1e3db06b180d6856b61e98fbf48b46b88acb1947
gaylonalfano <gaylon.alfano@gmail.com>
2018-10-04 14:41:46 +0800

    Editing models.py file to define what we're going to store in database. Updating db changes use python manage.py makemigrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# We need to think about what we actually want to save to the DB. We're going to have
+# users (authors of the posts) and the posts themselves. We already know how to create 
+# users in the admin. For now, we're just going make a post model. This will be a class
+# that inherits from django models class. 
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Let's create our post model
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
