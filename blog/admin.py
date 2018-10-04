aa49574b5b4571e6b51c5613390f532e884e5e62
gaylonalfano <gaylon.alfano@gmail.com>
2018-10-04 18:04:04 +0800

    Added Post model to admin site (admin.py) using admin.site.register(Post). Now can see Post data in admin.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+# Need to register our new Post model so that it shows up in admin page
+admin.site.register(Post)
