287d24eb3022d498c431d253e605f29dabe19528
gaylonalfano <gaylon.alfano@gmail.com>
2018-10-05 10:39:04 +0800

    Replaced UserCreationForm with new UserRegisterForm in our register view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm  -- Can remove after replacing with UserRegisterForm
 from django.contrib import messages
+# After creating a new form with a new email field, import the form:
+from .forms import UserRegisterForm
 
 # Let's create a register view to handle the logic for the register route
 def register(request):
@@ -9,7 +11,8 @@ def register(request):
     # There are different types of http requests (get, post, etc.)
     # Adding conditional to handle POST or GET requests
     if request.method == "POST":  # was the request a POST request?
-        form = UserCreationForm(request.POST)  # create a new form that has the data that's within request.POST
+        #form = UserCreationForm(request.POST)  # create a new form that has the data that's within request.POST
+        form = UserRegisterForm(request.POST)  # replaced w/ new form
         if form.is_valid():  # is it valid? UserCreationForm does all this
             form.save()  # saves the user data 
             username = form.cleaned_data.get('username')
@@ -17,5 +20,6 @@ def register(request):
             # Now redirect user to home page after success
             return redirect("blog-home")  # name of urlpattern for blog home page
     else:
-        form = UserCreationForm()  # creates a new instance of the form
+        #form = UserCreationForm()  # creates a new instance of the form
+        form = UserRegisterForm()  # replaced w/ new form
     return render(request, 'users/register.html', {'form': form})  
