1692155f205a09b0020bac199386c32f0a81e6b4
gaylonalfano <gaylon.alfano@gmail.com>
2018-10-05 12:52:34 +0800

    Created a new login.html template, told Django where to find it by passing arg to as_view(template_name=users/logout.html)

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
@@ -36,12 +37,20 @@ from users import views as user_views
 # module for our users app (like we did for blog app), but Corey had us hold off. So instead we're going to import
 # our view (register() in views.py) directly into our projects urls module (here).
 
+# We've completed the registration page but now we need to allow users to actually login.
+# Django has some default login and logout views so let's import them into this urls.py module
+# from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('', include('blog.urls'))  # if we go to /blog, we should now reference/send users to our blog.urls
+    path('', include('blog.urls')),  # if we go to /blog, we should now reference/send users to our blog.urls
     # When Django encounters include(), it chops off the included portion of the url ("/blog") and only sends the
     # remaining string to the included blog.urls module to get processed. Since nothing remainining, it just sends empty
     # string ''. Now over to blog.urls...
+
+    # After import django's default user login/logout views, time to create paths for them:
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
     
 ]
